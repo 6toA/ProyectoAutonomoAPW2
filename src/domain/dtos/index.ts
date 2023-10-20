9fd69d2804dc494e8931406320cb6977bff0aeff
JorgeZG13 <jzambrano020@gmail.com>
2023-10-20 18:19:59 -0500

    Jorge Zambrano

--- src/domain/dtos/index.ts
+++ src/domain/dtos/index.ts
@@ -7,3 +7,12 @@ export * from './encargado/update-encargado.dto'
 
 export * from './inventario/create-inventario.dto'
 export * from './inventario/update-inventario.dto'
+
+export * from './autor/create-autor.dto'
+export * from './autor/update-autor.dto'
+
+export * from './genero/create-genero.dto'
+export * from './genero/update-genero.dto'
+
+export * from './nacionalidad/create-nacionalidad.dto'
+export * from './nacionalidad/update-nacionalidad.dto'
--- src/presentacion/routes.ts
+++ src/presentacion/routes.ts
@@ -3,6 +3,9 @@ import { Router } from "express";
 import { NegocioRoutes } from "./negocio/routes";
 import { EncargadoRoutes } from "./encargado/routes";
 import { InventarioRoutes} from "./inventario/routes";
+import { AutorRoutes } from "./autor/routes";
+import { GeneroRoutes } from "./genero/routes";
+import { NacionalidadRoutes } from "./nacionalidad/routes";
 
 export class AppRoutes {
     static get routes(): Router {
@@ -11,7 +14,9 @@ export class AppRoutes {
         router.use('/api/negocios', NegocioRoutes.routes);
         router.use('/api/encargados', EncargadoRoutes.routes);
         router.use('/api/inventario', InventarioRoutes.routes);
-
+        router.use('/api/autores', AutorRoutes.routes);
+        router.use('/api/generos', GeneroRoutes.routes);
+        router.use('/api/Nacionalidades', NacionalidadRoutes.routes);
         return router;
     }
 }
